bdbf96cb8afd8226b7e8e4ae809d1bbc12c3032e
Ondrej Slejtr <o.slejtr@gmail.com>
2023-06-26 08:24:36 +0200

    add AWS setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { main } from "./main";
 
-export const handler = async (event: any, context) => {
+export const handler = async (event: any, context: any) => {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
 
     main();
